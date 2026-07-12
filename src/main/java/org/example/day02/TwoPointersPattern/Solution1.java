933e131f5a0a8599699b185f2dee7a7cfde3b155
mohcinelamtanez <lamtanezmohcine95@gmail.com>
2026-07-12 14:47:52 +0100

    add solution to sum of to numbers of a given 1 indexed array using the two pointers technique

---
+++ src/main/java/org/example/day02/TwoPointersPattern/Solution1.java
@@ -0,0 +1,26 @@
+package org.example.day02.TwoPointersPattern;
+
+/**
+ * @author USER
+ **/
+public class Solution1 {
+    public static int[] twoSumSortedArray(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1  ;
+
+        while (left < right) {
+
+            int sum = left + right ;
+
+            if (sum > target) {
+                right--;
+            } else if (sum< target) {
+                left++;
+            } else
+                return new int[]{left + 1, right +1};
+
+
+        }
+        return new int[]{};
+    }
+}
