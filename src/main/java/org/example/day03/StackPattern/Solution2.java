645a9883a372dd43e90b8b9a99fd6fa7d46ce6d7
mohcinelamtanez <lamtanezmohcine95@gmail.com>
2026-07-13 01:36:49 +0100

    solving the max sum of an array using the sliding window technique

---
+++ src/main/java/org/example/day03/StackPattern/Solution2.java
@@ -0,0 +1,34 @@
+package org.example.day03.StackPattern;
+
+/**
+ * @author USER
+ **/
+public class Solution2 {
+    public  static int maxSumSubArrayOfSizek( int[] nums , int k ) {
+
+        int currentSum =  0 ;
+
+        for( int i = 0 ; i<k ; i++) {
+            currentSum += nums[i] ;
+        }
+
+        int maxSum = currentSum ;
+
+        int left = 0 ;
+        int right = k ;
+
+        while(right < nums.length ) {
+
+            currentSum -= nums[left]  ;
+            currentSum += nums[right] ;
+
+            if(currentSum > maxSum) {
+                maxSum = currentSum ;
+            }
+
+            left++ ;
+            right++ ;
+        }
+        return maxSum  ;
+    }
+}
